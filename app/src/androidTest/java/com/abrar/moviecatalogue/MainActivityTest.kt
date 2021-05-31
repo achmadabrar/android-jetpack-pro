e2eec8f67a941a4323c0e23e9c774da62ceae96b
achmad <achmad.abrar@bridestory.com>
2021-06-01 06:08:20 +0700

    edit instrumenttest

--- app/src/androidTest/java/com/abrar/moviecatalogue/MainActivityTest.kt
+++ app/src/androidTest/java/com/abrar/moviecatalogue/MainActivityTest.kt
@@ -1,8 +1,8 @@
 package com.abrar.moviecatalogue
 
 import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
@@ -10,7 +10,8 @@ import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.abrar.moviecatalogue.utils.Dummy
 import com.abrar.moviecatalogue.utils.EspressoIdlingResource
@@ -20,7 +21,10 @@ import org.junit.Rule
 import org.junit.Test
 
 class MainActivityTest {
-   
+
+    private val dummyMovie = Dummy.generateDataMovieDummy()
+    private val dummyTvShow = Dummy.generateDataTvShowDummy()
+
     @get:Rule
     var activityRule = ActivityScenarioRule(MainActivity::class.java)
 
@@ -34,62 +38,76 @@ class MainActivityTest {
         IdlingRegistry.getInstance().unregister(EspressoIdlingResource.espressoTestIdlingResource)
     }
 
-    //menampilkan data movie dan scroll ke posisi ke 7
+    //menampilkan data movie dan scroll ke posisi ke 10
     @Test
     fun loadMovies() {
-        Espresso.onView(withId(R.id.recycler_movies)).check(matches(isDisplayed()))
-        Espresso.onView(withId(R.id.recycler_movies)).perform(
+        onView(withId(R.id.recycler_movies)).check(matches(isDisplayed()))
+        onView(withId(R.id.recycler_movies)).perform(
             RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
-                7
+                dummyMovie.size
             )
         )
     }
 
-    //menampilkan data movie dan cek terdapat data atau tidak dan scroll ke posisi ke 5 kemudian di klik kemudian cek view image berdasarkan id
+    //menampilkan data movie dan cek terdapat data atau tidak dan scroll ke posisi akhir dari dummy kemudian di klik pada data pertama.  Kemudian memastikan image poster, title dan deskripsi tampil sesuai yang diharapkan.
     @Test
     fun loadDetailMovie() {
-        Espresso.onView(withId(R.id.recycler_movies))
+        onView(withId(R.id.recycler_movies))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.onView(withId(R.id.recycler_movies))
-            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(5))
-        Espresso.onView(withId(R.id.recycler_movies))
+        onView(withId(R.id.recycler_movies))
+            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyMovie.size))
+        onView(withId(R.id.recycler_movies))
             .perform(
-                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(5,
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                    0,
                     ViewActions.click()
-                ))
-        Espresso.onView(withId(R.id.image_poster_movie))
+                )
+            )
+        onView(withId(R.id.image_poster_movie))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.pressBack()
+        onView(withId(R.id.text_title_movie))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withId(R.id.text_desc_movie))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
     }
 
+
+    //klik tvshow menu, cek data recycler tampil, scroll ke posisi akhir dari data dummy
     @Test
     fun loadTvShows() {
-        Espresso.onView(withId(R.id.tvShowMenu)).perform(click())
-        Espresso.onView(withId(R.id.recycler_tv_show)).check(matches(isDisplayed()))
-        Espresso.onView(withId(R.id.recycler_tv_show)).perform(
+        onView(withId(R.id.tvShowMenu)).perform(click())
+        onView(withId(R.id.recycler_tv_show)).check(matches(isDisplayed()))
+        onView(withId(R.id.recycler_tv_show)).perform(
             RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
-                4
+                dummyTvShow.size
             )
         )
     }
+
+    //menampilkan data tvShow dan cek terdapat data atau tidak dan scroll ke posisi ke akhir dari dummy kemudian di klik pada data pertama. Kemudian memastikan image poster, title dan deskripsi tampil sesuai yang diharapkan.
 
     @Test
     fun loadDetailTvShow() {
-        Espresso.onView(withId(R.id.tvShowMenu)).perform(click())
-        Espresso.onView(withId(R.id.recycler_tv_show))
+        onView(withId(R.id.tvShowMenu)).perform(click())
+        onView(withId(R.id.recycler_tv_show))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.onView(withId(R.id.recycler_tv_show))
-            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(5))
-        Espresso.onView(withId(R.id.recycler_tv_show))
+        onView(withId(R.id.recycler_tv_show))
+            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyTvShow.size))
+        onView(withId(R.id.recycler_tv_show))
             .perform(
-                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(5,
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                    0,
                     ViewActions.click()
-                ))
-        Espresso.onView(withId(R.id.text_title_tvshow))
+                )
+            )
+        onView(withId(R.id.image_poster_tvshow))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.onView(withId(R.id.text_desc_tvshow))
+        onView(withId(R.id.text_title_tvshow))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.pressBack()
+        onView(withId(R.id.text_desc_tvshow))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
     }
 
 }
